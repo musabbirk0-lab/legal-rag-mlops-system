886967f1af3423df319c033158468d06e86221d6
Musabbir Ahmed Khan <musabbirk0@gmail.com>
2025-11-24 04:48:26 -0800

    Update ingest_dag.py

--- airflow/dags/ingest_dag.py
+++ airflow/dags/ingest_dag.py
@@ -1,1 +1,16 @@
 
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+from app.ingest import ingest
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 1, 1),
+    'retries': 1,
+}
+
+dag = DAG('ingest_dag', default_args=default_args, schedule_interval='@daily')
+
+ingest_task = PythonOperator(task_id='ingest_pdfs', python_callable=ingest, dag=dag)
