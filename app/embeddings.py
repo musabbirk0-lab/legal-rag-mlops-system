0a1ba088bc5f0b8617063b322a054be727d38be2
Musabbir Ahmed Khan <musabbirk0@gmail.com>
2025-11-24 04:42:00 -0800

    Update embeddings.py

--- app/embeddings.py
+++ app/embeddings.py
@@ -1,1 +1,14 @@
+import os
+USE_OPENAI = os.getenv("USE_OPENAI", "false").lower() == 'true'
 
+if USE_OPENAI:
+    import openai
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    def get_embeddings(texts):
+        res = openai.Embedding.create(model="text-embedding-3-small", input=texts)
+        return [r['embedding'] for r in res['data']]
+else:
+    from sentence_transformers import SentenceTransformer
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    def get_embeddings(texts):
+        return model.encode(texts, show_progress_bar=False, convert_to_numpy=True)
